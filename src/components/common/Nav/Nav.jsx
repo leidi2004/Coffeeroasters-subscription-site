610efe07dc80e842a4343d989407070030abd540
Leidi <leididelapuente3@gmail.com>
2024-01-12 23:12:26 -0500

    Desktop nav

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 import logo from "../../../assets/img/shared/desktop/logo.svg"
 
 export const Nav = () => {
-  const [mobileMenu, setMobileMenu] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(true);
   
   const menuClass = mobileMenu ? styles.nav__menu_display : styles.nav__menu_hidden;
   return (
@@ -17,10 +17,10 @@ export const Nav = () => {
           <span className={styles.button__lines}></span>
           <span className={styles.button__lines}></span>
         </button>
-        <ul className={menuClass}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about-us">About Us</Link></li>
-            <li><Link to="/create-plan">Create Your Plan</Link></li>
+        <ul className={styles.nav__links}>
+            <li><Link to="/" className={styles.nav__link}>Home</Link></li>
+            <li><Link to="/about-us" className={styles.nav__link}>About Us</Link></li>
+            <li><Link to="/create-plan" className={styles.nav__link}>Create Your Plan</Link></li>
         </ul>
     </nav>
   )
