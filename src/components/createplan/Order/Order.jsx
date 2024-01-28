94047ab069fcca8520d8a961b41bba63e1905829
Leidi <leididelapuente3@gmail.com>
2024-01-27 22:34:15 -0500

    Order Section Layout

--- src/components/createplan/Order/Order.jsx
+++ src/components/createplan/Order/Order.jsx
@@ -13,20 +13,16 @@ export const Order = () => {
 				<h3 className={styles.card__title}>Order Summary</h3>
 				<p className={styles.card__text}>
 					<q>
-						I drink coffee as
-						<span className={styles.card__span}>{order.coffee}</span>, with a
+						I drink coffee as{' '}
+						<span className={styles.card__span}>{order.coffee}</span>, with a{' '}
 						<span className={styles.card__span}>{order.type}</span> type of
-						bean. <span className={styles.card__span}>{order.amount}</span>
-						ground ala
-						<span className={styles.card__span}>{order.grind}</span>, sent to me
-						<span className={styles.card__span}>{order.plan}</span>.
+						bean. <span className={styles.card__span}>{order.amount}</span>{' '}
+						ground ala <span className={styles.card__span}>{order.grind}</span>,
+						sent to me <span className={styles.card__span}>{order.plan}</span>.
 					</q>
 				</p>
 			</div>
-
-      <button>
-        Create Plan!
-      </button>
+			<button className={styles.section__button}>Create Plan!</button>
 		</section>
 	);
 };
